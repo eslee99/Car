32b7cf5e33818402e015de9ceef141281e24c7bd
eslee99 <leeshijing_99@hotmail.co.uk>
2019-10-31 23:46:47 +0800

    Solved[MainActivity.kt, line 23]: Casting problem.

--- app/src/main/java/com/example/car/MainActivity.kt
+++ app/src/main/java/com/example/car/MainActivity.kt
@@ -14,24 +14,21 @@ class MainActivity : AppCompatActivity() {
         buttonCalculate.setOnClickListener(){
             calculateLoan()
         }
-        buttonReset.setOnClickListener(){
-            resetInput(buttonReset)
-        }
 
     }
     private fun calculateLoan(){
         val car_price=editTextCarPrice.text.toString().toInt()
         val down_payment=editTextDownPayment.text.toString().toInt()
         val loan_period=editTextLoanPeriod.text.toString().toInt()
-        val interest_rate=editTextInterestRate.text.toString().toInt()
+        val interest_rate=editTextInterestRate.text.toString().toFloat()
 
         val loan=car_price-down_payment
         val interest=loan*interest_rate*loan_period
         val monthlyRepayment=(loan+interest)/loan_period/12
 
-        textViewCarLoan.text=getString(R.string.loan) + ": ${loan}"
-        textViewInterest.text="Interest" + ": ${interest}"
-        textViewMonthlyRepayment.text="Monthly Repayment" + "${monthlyRepayment}"
+        textViewCarLoan.text=getString(R.string.loan,loan.toFloat())
+        textViewInterest.text=getString(R.string.interest,interest.toFloat())
+        textViewMonthlyRepayment.text=getString(R.string.montly_repayment,monthlyRepayment.toFloat())
     }
     private fun resetInput(view: View){
 
